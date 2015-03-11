149783a3277b4cfb1da9afda693808fc5e01e869
Michael Ludwig <mdludwig@umass.edu>
2015-03-11 17:07:54 -0400

    Added script

---
+++ run.sh
@@ -0,0 +1,25 @@
+tables=(
+    people
+    genres
+    mpaa_ratings
+    users
+    actors
+    directors
+    producers
+    movies
+    friends
+    activities
+    movies_actors
+    movies_directors
+    movies_producers
+    movies_genres
+    ratings
+)
+
+for (( i = ${#tables[@]}-1; i >= 0; i-- )); do
+    mysql -h cs445sql --user=$1 --password=$2 FLP -e "drop table ${tables[i]}"
+done
+
+for t in ${tables[@]}; do
+    mysql -h cs445sql --user=$1 --password=$2 FLP -e "source sql/${t}.sql"
+done
